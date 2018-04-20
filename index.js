7a1aaceec3b1cf00e8be4078c96c4c9919290205
Max de Vries <max.devries.1995@gmail.com>
2018-04-20 12:34:42 +0200

    Add express and express-socket.io session functionality

--- index.js
+++ index.js
@@ -3,30 +3,42 @@ var app = express();
 var http = require('http').Server(app);
 var yql = require('yql');
 var io = require('socket.io')(http);
+var session = require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+});
+var sharedsession = require('express-socket.io-session');
 require('dotenv').config({ path: './vars.env' });
 
 app.use(express.static('public'));
+app.use(session);
 app.set('view engine', 'ejs');
+
+// API updates each hour with a 35 min delay for some reason (e.g. 10.00 am update updates at 10.35)
+
+// Polling help: https://github.com/vpdeva/long-poll/blob/master/server.js
 
 var apiClientId = process.env.API_CLIENT_ID;
 var apiClientSecret = process.env.API_CLIENT_SECRET;
 
-var queries = {
-  getAllFromAmsterdam: 'select * from weather.forecast where woeid = 727232 and u="c"'
-};
-
-// Polling:
-// help: https://github.com/vpdeva/long-poll/blob/master/server.js
+var query = function (city) {
+  return `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city.toLowerCase()}') and u='c'`;
+}
 
 var pollingTimer;
-var connectedClients = [];
 
-var pollingLoop = function () {
+var cities = [];
+
+var connectedSockets = [];
+var database = [];
+
+var pollingLoop = function (city) {
   // Make a new yahoo query:
-  var query = new yql(queries.getAllFromAmsterdam);
+  var YQL = new yql(query(city));
 
   // Execute this query:
-  query.exec(function(err, data) {
+  YQL.exec(function(err, data) {
     if (err) throw err;
 
     var results = data.query.results.channel;
@@ -36,6 +48,7 @@ var pollingLoop = function () {
 
     // Get the wanted weather data as an object:
     var weatherData = {
+      location: results.location,
       units: results.units,
       lastBuildDate: results.lastBuildDate,
       wind: results.wind,
@@ -47,9 +60,11 @@ var pollingLoop = function () {
     };
 
     // When there are clients connected:
-    if (connectedClients.length) {
+    if (database.length) {
       // Update the pollingTimer:
-      pollingTimer = setTimeout(pollingLoop, 60000);
+      pollingTimer = setTimeout(function () {
+        pollingLoop(city);
+      }, 36000000);
 
       // Update the sockets:
       updateSockets(weatherData);
@@ -58,33 +73,36 @@ var pollingLoop = function () {
   });
 }
 
-io.sockets.on('connection', function (socket) {
-  console.log('a user connected');
-  connectedClients.push(socket);
-  console.log(`number of connections: ${connectedClients.length}`);
+io.use(sharedsession(session, {
+    autoSave:true
+}));
 
-  // When there are clients connected:
-  if (connectedClients.length) {
-    pollingLoop();
-  }
+io.on('connection', function (socket) {
+  console.log(`User ${socket.handshake.session.client.name} connected`);
+
+  connectedSockets.push(socket);
+
+  var socketIndex = database.indexOf(socket.handshake.session.client);
+
+  cities.push(socket.handshake.session.client.city);
+
+  cities.forEach(function (city) {
+    pollingLoop(city);
+  });
 
   socket.on('disconnect', function () {
-    var socketIndex = connectedClients.indexOf(socket);
-    console.log(`user ${socketIndex} disconnected`);
-    if (socketIndex >= 0) {
-      connectedClients.splice(socketIndex, 1);
+    if (socket.handshake.session.client) {
+      console.log(`User ${socket.handshake.session.client.name} disconnected`);
+      delete socket.handshake.session.client;
+      database.splice(socketIndex, 1);
     }
+    console.log(`Clients in database: ${database.length}`);
   });
 });
 
 var updateSockets = function (weatherData) {
-  // Add the current time as a property in the weather data object:
-  weatherData.time = new Date();
-
-  // For each connected client:
-  connectedClients.forEach(function (client) {
-    // Send the updated weather data:
-    client.emit('update', weatherData);
+  connectedSockets.forEach(function (socket) {
+    socket.emit('update', weatherData);
   });
 }
 
@@ -92,6 +110,19 @@ var updateSockets = function (weatherData) {
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/weather', function (req, res) {
+
+  req.session.client = {
+    id: req.session.id,
+    name: req.query.name,
+    city: req.query.city
+  };
+
+  database.push(req.session.client);
+
+  res.render('weather');
 });
 
 http.listen(process.env.LOCALHOST);
